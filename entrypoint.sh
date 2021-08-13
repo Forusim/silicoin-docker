8ab1e537972eb44c5cbb1ebbb0d46be28b78b957
Forusim <forusim@web.de>
2021-08-13 22:32:25 +0200

    Improve sleep and shutdown

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,8 @@ if [[ $(silicoin keys show | wc -l) -lt 5 ]]; then
     else
       silicoin init && silicoin keys add -f ${keys}
     fi
+    
+    sed -i 's/localhost/127.0.0.1/g' ~/.silicoin/mainnet/config/config.yaml
 else
     for p in ${plots_dir//:/ }; do
         mkdir -p ${p}
@@ -30,8 +32,6 @@ else
         fi
         silicoin plots add -d ${p}
     done
-
-    sed -i 's/localhost/127.0.0.1/g' ~/.silicoin/mainnet/config/config.yaml
 
     if [[ ${farmer} == 'true' ]]; then
       silicoin start farmer-only
@@ -48,4 +48,13 @@ else
     fi
 fi
 
-while true; do sleep 30; done;
+finish () {
+    echo "$(date): Shutting down silicoin"
+    silicoin stop all
+    exit 0
+}
+
+trap finish SIGTERM SIGINT SIGQUIT
+
+sleep infinity &
+wait $!
