702fa28d610e47e32ae298f0e00020ad745c14c8
Forusim <forusim@web.de>
2021-10-27 00:40:17 +0200

    Update names for v1.0.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,53 +4,53 @@ cd /silicoin-blockchain
 
 . ./activate
 
-if [[ $(silicoin keys show | wc -l) -lt 5 ]]; then
+if [[ $(sit keys show | wc -l) -lt 5 ]]; then
     if [[ ${keys} == "generate" ]]; then
       echo "to use your own keys pass them as a text file -v /path/to/keyfile:/path/in/container and -e keys=\"/path/in/container\""
-      silicoin init && silicoin keys generate
+      sit init && sit keys generate
     elif [[ ${keys} == "copy" ]]; then
       if [[ -z ${ca} ]]; then
         echo "A path to a copy of the farmer peer's ssl/ca required."
         exit
       else
-      silicoin init -c ${ca}
+      sit init -c ${ca}
       fi
     elif [[ ${keys} == "type" ]]; then
-      silicoin init
-      echo "Call from docker shell: silicoin keys add"
+      sit init
+      echo "Call from docker shell: sit keys add"
       echo "Restart the container after mnemonic input"
     else
-      silicoin init && silicoin keys add -f ${keys}
+      sit init && sit keys add -f ${keys}
     fi
     
-    sed -i 's/localhost/127.0.0.1/g' ~/.silicoin/mainnet/config/config.yaml
+    sed -i 's/localhost/127.0.0.1/g' ~/.sit/mainnet/config/config.yaml
 else
     for p in ${plots_dir//:/ }; do
         mkdir -p ${p}
         if [[ ! "$(ls -A $p)" ]]; then
             echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
         fi
-        silicoin plots add -d ${p}
+        sit plots add -d ${p}
     done
 
     if [[ ${farmer} == 'true' ]]; then
-      silicoin start farmer-only
+      sit start farmer-only
     elif [[ ${harvester} == 'true' ]]; then
       if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
         echo "A farmer peer address, port, and ca path are required."
         exit
       else
-        silicoin configure --set-farmer-peer ${farmer_address}:${farmer_port}
-        silicoin start harvester
+        sit configure --set-farmer-peer ${farmer_address}:${farmer_port}
+        sit start harvester
       fi
     else
-      silicoin start farmer
+      sit start farmer
     fi
 fi
 
 finish () {
-    echo "$(date): Shutting down silicoin"
-    silicoin stop all
+    echo "$(date): Shutting down sit"
+    sit stop all
     exit 0
 }
 
